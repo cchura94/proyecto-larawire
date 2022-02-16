02b068026832e0098ceb21ffb780876f31cdbcc6
cchura94 <cchura.cpc@gmail.com>
2022-02-15 20:08:50 -0400

    BASE DE DATOS

---
+++ app/Http/Livewire/CategoriaComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CategoriaComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.categoria-component');
+    }
+    
+}
---
+++ app/Models/Pedido.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pedido extends Model
+{
+    use HasFactory;
+
+    public function productos()
+    {
+        return $this->belongsToMany(Producto::class)->withPivot("cantidad");
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
