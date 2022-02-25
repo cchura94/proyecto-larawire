0334d69e5a11e6be2bcfe9572032a402b44bbc43
cchura94 <cchura.cpc@gmail.com>
2022-02-24 20:21:16 -0400

    Pedidos

--- app/Http/Livewire/Pedido/NuevoPedidoComponent.php
+++ app/Http/Livewire/Pedido/NuevoPedidoComponent.php
@@ -3,11 +3,45 @@
 namespace App\Http\Livewire\Pedido;
 
 use Livewire\Component;
+use App\Models\Producto;
 
 class NuevoPedidoComponent extends Component
 {
+    // atributos
+    public $productos;
+    public $carrito;
+
+    // metodos
     public function render()
     {
         return view('livewire.pedido.nuevo-pedido-component');
+    }
+
+    // cargar informacion cuando cargue el componente
+    public function mount()
+    {
+        $this->productos = Producto::all();
+        $this->carrito = array();
+        // $this->productos = ["Mesa", "Silla", "Escritorio"];                
+    }
+
+    public function addCarrito($prod){
+        $p = [
+            "id" => $prod["id"],
+            "nombre" => $prod["nombre"],
+            "precio" => $prod["precio"],
+            "cantidad" => 1
+        ];
+        // falta reducir el stock del inv
+        array_push($this->carrito, $p);
+    }
+
+    public function quitarCarrito($id){
+        $index = 0;
+        foreach ($this->carrito as $prod)
+        {
+            if ($prod['id']==$id) unset($this->carrito[$index]);
+            $index++;
+        }
     }
 }
--- resources/views/livewire/pedido/nuevo-pedido-component.blade.php
+++ resources/views/livewire/pedido/nuevo-pedido-component.blade.php
@@ -5,10 +5,18 @@
             <div class="card">
                 <div class="card-body">
                     <h5>Datos del pedido</h5>
-                    fecha_pedido
-cliente_id
-user_id
-estado
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h5>Fecha: {{ date('d M, Y - H:i:s') }}</h5>
+                        </div>
+                        <div class="col-md-5">
+                            <h4>Cuenta: {{ Auth::user()->name }} - {{ Auth::user()->email }}</h4>
+                        </div>
+                        <div class="col-md-3">
+                            <h2>En Proceso</h2>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -16,6 +24,33 @@ estado
             <div class="card">
                 <div class="card-body">
                     <h5>Lista de Productos</h5>
+                    <table class="table table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NOMBRE</th>
+                                <th>PRECIO</th>
+                                <th>STOCK</th>
+                                <th>ACCION</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($productos as $prod)
+                            <tr>
+                                <td>{{ $prod->id }}</td>
+                                <td>{{ $prod->nombre }}</td>
+                                <td>{{ $prod->precio }}</td>
+                                <td>{{ $prod->cantidad }}</td>
+                                <td>
+                                    <button wire:click="addCarrito({{ $prod }})" class="btn btn-info btn-xs">
+                                        <i class="fa fa-plus"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+
+                    </table>
                 </div>
             </div>
         </div>
@@ -25,6 +60,28 @@ estado
                     <div class="card">
                         <div class="card-body">
                             <h5>Carrito</h5>
+                            <table class="table">
+                                <tr>
+                                    <td>NOM</td>
+                                    <td>P.U.</td>
+                                    <td>Cant.</td>
+                                    <td>S.T.</td>
+                                    <td></td>
+                                </tr>
+                                @foreach ($carrito as $p)
+                                <tr>
+                                    <td>{{$p['nombre']}}</td>
+                                    <td>{{$p['precio']}}</td>
+                                    <td>{{$p['cantidad']}}</td>
+                                    <td>{{$p['precio'] * $p['cantidad'] }}</td>
+                                    <td>
+                                        <button class="btn btn-danger btn-xs" wire:click="quitarCarrito({{$p['id']}})">
+                                            <i class="fa fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -42,7 +99,7 @@ estado
                         </div>
                     </div>
                 </div>
-            </div>            
+            </div>
         </div>
 
     </div>
